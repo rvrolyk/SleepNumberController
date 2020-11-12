af74a12f163bc083053c0f59e8a917a80c468bc8
Brad Sileo <brad@sileo.name>
2020-11-12 17:35:05 -0500

    Added support for child devices with Outlet and Underbed Light (#2)
    * Added support for child devices with Outlet and Underbed Light
    
    * Fixed Component status so these can be manipulated outside the Parent Bed Side.
    
    Also fixed children to only happen on "Presence" device
    
    * Cleanup approach to finding single children
    
    * Remove superfluous tags from old ST compatibility cut and paste lazy-ness
    
    Co-authored-by: rvrolyk <47088852+rvrolyk@users.noreply.github.com>

--- SleepNumberController_Driver.groovy
+++ SleepNumberController_Driver.groovy
@@ -109,6 +109,7 @@ metadata {
       input name: "footWarmerLevel", type: "enum", title: "Warmer level for 'on'", description: "Only valid for device type that is 'foot warmer'", options: HEAT_TEMPS.collect{ it.key }, defaultValue: "Medium"
       input name: "footWarmerTimer", type: "enum", title: "Warmer duration for 'on'", description: "Only valid for device type that is 'foot warmer'", options: HEAT_TIMES.collect{ it.key }, defaultValue: "30m"
       input name: "enableSleepData", type: "bool", title: "Enable sleep data collection", defaultValue: false
+      input name: "enableChildDevices", type: "bool", title: "Enable child devices for switches (Presence device only)", defaultValue: false
     }
   }
 }
@@ -125,6 +126,9 @@ def logsOff() {
 
 def updated() {
   debug "updated()"
+   if (enableChildDevices) {
+      manageChildren()      
+  }
   if (logEnable) {
     runIn(1800, logsOff)
   }
@@ -140,6 +144,82 @@ def updated() {
   // Poll right after updated / installed
   poll()
 }
+
+def uninstalled() {
+   deleteChildDevice(getChildDNI("outlet"))
+   deleteChildDevice(getChildDNI("underbedlight"))
+}
+
+def manageChildren() {
+    switch (state.type) {
+        case "presence":
+            def outlet = getOutlet()
+            if (!outlet) {
+                outlet = addChildDevice('hubitat','Generic Component Switch', getChildDNI("outlet"),
+                                        [ label: getChildName("Outlet")])
+                log.info("Created Outlet child device")
+            }
+            def underbed = getUnderbed()
+            if (!underbed) {
+                outlet = addChildDevice('hubitat','Generic Component Switch', getChildDNI("underbedlight"),
+                                        [ label: getChildName("Underbed Light")])
+                log.info("Created Underbed light child device")
+            }
+            break
+    }
+}
+
+def getOutlet() {
+    return getChildDevice(getChildDNI("outlet"))
+}
+
+def getUnderbed() {
+    return getChildDevice(getChildDNI("underbedlight"))
+}
+
+
+def getChildName(name) {
+    return "Sleep Number ${state.side} - ${name}"
+}
+
+def getChildDNI(name) {
+	return device.deviceNetworkId + "-" + name
+}
+
+def getChildDNI(type, name) {
+    return device.deviceNetworkId + + "-${type}-${name}"
+}
+
+def componentRefresh(device) {
+    log.warn("Component Refresh not implemented")
+}
+
+def componentOn(device) {    
+    def o = getOutlet()  
+    def u = getUnderbed()  
+    if (device.deviceNetworkId == o?.deviceNetworkId) {     
+        setOutletState('On')
+        device.sendEvent([[name:"switch", value: "on"]])
+    }
+    else if (device.deviceNetworkId == u?.deviceNetworkId) {
+        setUnderbedLightState('On')
+        device.sendEvent([[name:"switch", value: "on"]])
+    }
+}
+
+def componentOff(device) {
+    def o = getOutlet()   
+     def u = getUnderbed()  
+    if (device.deviceNetworkId == o?.deviceNetworkId) {        
+        setOutletState('Off')
+        device.sendEvent([[name:"switch", value: "off"]])
+    }
+    else if (device.deviceNetworkId == u?.deviceNetworkId) {
+        setUnderbedLightState('Off')
+        device.sendEvent([[name:"switch", value: "off"]])
+    }
+}
+
 
 def parse(String description) {
   debug "parse() - Description: ${description}"
@@ -384,7 +464,7 @@ def setOutletState(state) {
   sendToParent "setOutletState", state
 }
 
-def setUnderbedLightState(state, timer = 0, brightness = "high") {
+def setUnderbedLightState(state, timer = "Forever", brightness = "High") {
   debug "setUnderbedLightState(${state}, ${timer}, ${brightness})"
   if (state == null || !UNDERBED_LIGHT_STATES.contains(state)) {
     log.error "Invalid state ${state}"
